5573f1f1672d38193b95678d14c9e1b7589a6c1e
unknown <priyajain7988@gmail.com>
2025-06-23 12:43:45 +0530

    Added Day 9 folder

---
+++ Day9/eventListener.js
@@ -0,0 +1,33 @@
+/////////////////////////////////////////// eventListeners //////////////////////////////////////////////////////////
+const h1 = document.querySelector("h1");
+
+// h1.addEventListener("click",()=>{
+//     h1.classList.toggle("background");
+// })
+
+// h1.addEventListener("click",(e)=>{
+//     console.log(e);
+//     h1.classList.toggle("background");
+// })
+h1.addEventListener("click",(e)=>{
+    console.log(e.target); // return element at which the event occur
+    h1.classList.toggle("background");
+    console.log("h1 clicked")
+})
+
+//////////////////////////////////////// event bubling ////////////////////////////////////////////
+// Event Flow: The event goes from the innermost element to the outermost, like child → parent → grandparent.
+// Default Behavior: JavaScript event listeners react in the bubbling phase by default (unless you specify otherwise).
+// Real-life Example: Imagine clicking a button inside a form. The click event happens on the button first, then moves up to the form, and then maybe the whole webpage (document).
+// Use Case: You can listen for events on a parent instead of each child. This is super useful for dynamic elements!
+// Stop Bubbling (Optional): You can use event.stopPropagation() if you don’t want the event to bubble up further.
+
+// h1.children[0].addEventListener("click",(e)=>{
+//     console.log("span clicked");
+// })
+
+h1.children[0].addEventListener("click",(e)=>{
+    e.stopPropagation(); // use this in child to stop propogation.
+    console.log("span clicked");
+})
+
